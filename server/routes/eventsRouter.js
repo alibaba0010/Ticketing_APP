593c771169becb6447c0b3c3201efac885421d1e
alibaba0010 <zakariyahali100@gmail.com>
2024-07-06 19:38:01 +0100

    adding event funcs

--- server/routes/eventsRouter.js
+++ server/routes/eventsRouter.js
@@ -1,21 +1,17 @@
+import { authentication, validateRequest, validateTicket } from "utils/auth";
+import { Router } from "express";
 import {
-   authentication,
-   validateRequest,
-   validateTicket,
- } from "utils/auth";
- import { Request, Response, Router } from "express";
- import {
-   createTicket,
-   getTicketWithId,
-   updateTicket,
-   getTickets,
- } from "../controllers/tickets.controller.js";
- 
- const eventRouter = Router();
- 
- eventRouter
-   .post("/", authentication, validateTicket, validateRequest, createTicket)
-   .get("/", getTickets)
-   .get("/:id", getTicketWithId)
-   .patch("/:id", authentication, validateTicket, updateTicket);
- export default eventRouter;
+  createEvent,
+  createTicket,
+  getTicketWithId,  
+  getTickets,
+} from "../controllers/tickets.controller.js";
+
+const eventRouter = Router();
+
+eventRouter
+  .post("/", authentication, validateTicket, validateRequest, createTicket)
+  .get("/", getTickets)
+  .get("/:id", getTicketWithId)
+  .patch("/:id", authentication, validateTicket, updateTicket);
+export default eventRouter;
