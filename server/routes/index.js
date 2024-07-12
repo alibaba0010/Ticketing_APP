83901b84a042271fb29a5eebad762a6a1a08714c
alibaba0010 <zakariyahali100@gmail.com>
2024-07-12 22:47:26 +0100

    fixing mongoose error issue overwrite Ticket

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,8 @@
 import {Router} from "express";
 import userRouter from "./userRouter";
 import eventRouter from "./eventsRouter";
-const ticketRouter = Router();
-ticketRouter
+const indexRouter = Router();
+indexRouter
 .use("/users", userRouter)
 .use("/events", eventRouter);
-export default ticketRouter;
+export default indexRouter;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 import express, { json } from "express";
-import ticketRouter from "./routes/index";
+import indexRouter from "./routes/index";
 import  connecttoDB  from "./utils/db";
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(json());
 
-ticketRouter(app);
+indexRouter(app);
 const uri = process.env.MONGO_URL;
 app.listen(PORT, async () => {
   await connecttoDB();
